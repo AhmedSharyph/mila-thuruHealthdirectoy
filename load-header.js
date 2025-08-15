171a58fc42e5351e4032e5c6f33a63de7a4ab0c2
Ahmed Sharyph <39231530+AhmedSharyph@users.noreply.github.com>
2025-08-16 00:12:16 +0500

    Update load-header.js

--- load-header.js
+++ load-header.js
@@ -4,3 +4,13 @@ fetch('header.html')
     document.getElementById('header-placeholder').innerHTML = data;
   })
   .catch(err => console.error('Error loading header:', err));
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const currentPath = window.location.pathname;
+  const addContactLink = document.querySelector('#navbarNav a[href="https://ahmedsharyph.github.io/shaviyanihealthdirectory/add-contact.html"]');
+
+  if (addContactLink && currentPath.endsWith('https://ahmedsharyph.github.io/shaviyanihealthdirectory/add-contact.html')) {
+    addContactLink.style.display = 'none';
+  }
+});
