4c60f55a8671653a34eada8e63e620b58c0e2e64
Ahmed Sharyph <39231530+AhmedSharyph@users.noreply.github.com>
2025-08-15 23:58:45 +0500

    Update hide-add-contact.js

--- hide-add-contact.js
+++ hide-add-contact.js
@@ -1,4 +1,3 @@
-<script>
 document.addEventListener('DOMContentLoaded', () => {
   const currentPath = window.location.pathname;
   const addContactLink = document.querySelector('#navbarNav a[href="add-contact.html"]');
