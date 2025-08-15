15b7e33522cd41049d6062729262ce781d0db1e4
Ahmed Sharyph <39231530+AhmedSharyph@users.noreply.github.com>
2025-08-16 00:12:36 +0500

    Delete hide-add-contact.js

--- hide-add-contact.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const currentPath = window.location.pathname;
-  const addContactLink = document.querySelector('#navbarNav a[href="https://ahmedsharyph.github.io/shaviyanihealthdirectory/add-contact.html"]');
-
-  if (addContactLink && currentPath.endsWith('https://ahmedsharyph.github.io/shaviyanihealthdirectory/add-contact.html')) {
-    addContactLink.style.display = 'none';
-  }
-});
