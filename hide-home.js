d59fdbc8d24400b7ad19800042cb4858850a282f
Ahmed Sharyph <39231530+AhmedSharyph@users.noreply.github.com>
2025-08-16 00:06:39 +0500

    Update hide-home.js

--- hide-home.js
+++ hide-home.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const currentPath = window.location.pathname;
-  const homeLink = document.querySelector('#navbarNav a[href="./"], #navbarNav a[href="index.html"]');
+  const homeLink = document.querySelector('#navbarNav a[href="https://ahmedsharyph.github.io/shaviyanihealthdirectory/"], #navbarNav a[href="https://ahmedsharyph.github.io/shaviyanihealthdirectory/index.html"]');
 
-  if (homeLink && (currentPath === '/shaviyanihealthdirectory/' || currentPath.endsWith('index.html'))) {
+  if (homeLink && (currentPath === '/shaviyanihealthdirectory/' || currentPath.endsWith('https://ahmedsharyph.github.io/shaviyanihealthdirectory/index.html'))) {
     homeLink.style.display = 'none';
   }
 });
